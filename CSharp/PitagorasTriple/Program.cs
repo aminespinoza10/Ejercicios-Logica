491cca7c8f3ec121d7e967d7d9cb5e666914d1c5
Amin Espinoza <amines@microsoft.com>
2020-08-07 16:08:49 -0500

    Ejercicio de Pitagoras triple en CSharp adaptado a .NET Core

---
+++ CSharp/PitagorasTriple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PitagorasTriple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejecución de aplicación");
+            EvaluateExpression(1000);
+        }
+
+        static void EvaluateExpression(int desiredNumber)
+        {
+            int a;
+
+            for (a = 1; a <= desiredNumber / 3; a++)
+            {
+                int b;
+                for (b = a + 1; b <= desiredNumber / 2; b++)
+                {
+                    int c = desiredNumber - a - b;
+                    if (a * a + b * b == c * c)
+                    {
+                        Console.WriteLine($"{a}, {b}, {c}");
+                    }
+                }
+            }
+        }
+    }
+}
