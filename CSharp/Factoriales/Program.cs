e6afb667082a03c63432509f4af9cfa11b4de181
Amin Espinoza <amines@microsoft.com>
2020-08-07 13:18:58 -0500

    Ejercicio de Factoriales en CSharp adaptado a .NET Core

---
+++ CSharp/Factoriales/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Factoriales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejecución de aplicación");
+            SearchFactorial(600851475143);
+        }
+
+        static void SearchFactorial(long evaluatedNumber)
+        {
+            long k = 2;
+            while (k * k <= evaluatedNumber)
+            {
+                if (evaluatedNumber % k == 0)
+                {
+                    evaluatedNumber /= k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+
+            Console.Write("Tu resultado es: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(evaluatedNumber);
+        }
+    }
+}
---
+++ CSharp/Factoriales/README.md
@@ -0,0 +1,7 @@
+## Factoriales
+
+Los factores primarios de 13195 son 5, 7, 13 y 29.
+
+¿Cuál es el factor primario más grande del número 600851475143?
+
+Enlace de referencia: [Aquí](https://projecteuler.net/problem=3)
