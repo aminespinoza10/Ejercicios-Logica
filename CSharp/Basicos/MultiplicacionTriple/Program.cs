5b542ad3cf0ccd0750c17ba80ef240c00734f420
aminespinoza10 <amin.espinoza@outlook.com>
2022-06-18 08:50:06 -0500

    multiplicación triple actualizado

--- CSharp/Basicos/MultiplicacionTriple/Program.cs
+++ CSharp/Basicos/MultiplicacionTriple/Program.cs
@@ -1,25 +1,14 @@
-﻿using System;
+﻿Console.Write("Hola! Comencemos por recibir el primer número: ");
+int numberOne = Convert.ToInt16(Console.ReadLine());
+Console.Write("Ahora dame el segundo número: ");
+int numberTwo = Convert.ToInt16(Console.ReadLine());
+Console.Write("Vamos por el tercer número: ");
+int numberThree = Convert.ToInt16(Console.ReadLine());
+int operationResult = Multiplicate(numberOne, numberTwo, numberThree);
+Console.WriteLine($"Tu resultado es {operationResult}");
 
-namespace MultiplicacionTriple
+int Multiplicate(int firstNumber, int secondNumber, int thirdNumber)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Hola! Comencemos por recibir el primer número:");
-            int numberOne = Convert.ToInt16(Console.ReadLine());
-            Console.Write("Ahora dame el segundo número");
-            int numberTwo = Convert.ToInt16(Console.ReadLine());
-            Console.Write("Vamos por el tercer número");
-            int numberThree = Convert.ToInt16(Console.ReadLine());
-            int operationResult = Multiplicate(numberOne, numberTwo, numberThree);
-            Console.WriteLine($"Tu resultado es {operationResult}");
-        }
-
-        static int Multiplicate(int firstNumber, int secondNumber, int thirdNumber)
-        {
-            int result = (firstNumber * secondNumber) * thirdNumber;
-            return result;
-        }
-    }
+    int result = (firstNumber * secondNumber) * thirdNumber;
+    return result;
 }
