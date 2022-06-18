ae96f1afc6808c4bdf88ff4966b50c68b190a820
aminespinoza10 <amin.espinoza@outlook.com>
2022-06-18 09:06:22 -0500

    tablas Multiplicación actualizado

--- CSharp/Basicos/tablasMultiplicacion/Program.cs
+++ CSharp/Basicos/tablasMultiplicacion/Program.cs
@@ -1,27 +1,16 @@
-﻿using System;
+﻿Console.WriteLine("Aplicación iniciada");
+Console.WriteLine("Escribe el numero que vas a querer multiplicar");
+int firstNumber = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Escribe cuantas veces vas a querer multiplicarlo");
+int secondNumber = Convert.ToInt16(Console.ReadLine());
+CreateIteration(firstNumber, secondNumber);
 
-namespace tablasMultiplicacion
+static void CreateIteration(int rootNumber, int loopNumber)
 {
-    class Program
+    
+    for (int i = 1; i < (loopNumber+1); i++)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Aplicación iniciada");
-            Console.WriteLine("Escribe el numero que vas a querer multiplicar");
-            int firstNumber = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Escribe cuantas veces vas a querer multiplicarlo");
-            int secondNumber = Convert.ToInt16(Console.ReadLine());
-            CreateIteration(firstNumber, secondNumber);
-        }
-
-        static void CreateIteration(int rootNumber, int loopNumber)
-        {
-            
-            for (int i = 1; i < (loopNumber+1); i++)
-            {
-                int resultNumber = rootNumber * i;
-                Console.WriteLine ($"{rootNumber} x {i} = {resultNumber}");
-            }
-        }
+        int resultNumber = rootNumber * i;
+        Console.WriteLine ($"{rootNumber} x {i} = {resultNumber}");
     }
 }
