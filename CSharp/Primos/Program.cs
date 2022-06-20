2353f7a14014f53bdc0d6d9c96d01a17a7444aff
aminespinoza10 <amin.espinoza@outlook.com>
2022-06-20 09:53:42 -0500

    Numeros primos actualizado

--- CSharp/Primos/Program.cs
+++ CSharp/Primos/Program.cs
@@ -1,58 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+﻿List<int> primesList = new List<int>();
 
-namespace Primos
-{
-    class Program
-    {
-        static List<int> primesList = new List<int>();
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Ejecución de aplicación");
+Console.WriteLine("Ejecución de aplicación");
             
-            Console.Write("Tu resultado es: ");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            GetPrimes();
-            //Console.WriteLine(finalResult);
-        }
+Console.Write("Tu resultado es: ");
+Console.ForegroundColor = ConsoleColor.Cyan;
+GetPrimes();
+//Console.WriteLine(finalResult);
 
-        static void GetPrimes()
+void GetPrimes()
+{
+    for (int i = 1; i < 2000000; i++)
+    {
+        if (isPrime(i))
         {
-            for (int i = 1; i < 2000000; i++)
-            {
-                if (isPrime(i))
-                {
-                    primesList.Add(i);
-                }
-            }
-            GetSumOfPrimes();
-        }
-
-        static bool isPrime(int number)
-        {
-            if (number == 1) return false;
-            if (number == 2) return true;
-
-            var boundary = (int)Math.Floor(Math.Sqrt(number));
-
-            for (int i = 2; i <= boundary; ++i)
-            {
-                if (number % i == 0) return false;
-            }
-
-            return true;
-        }
-
-        static void GetSumOfPrimes()
-        {
-            int finalSum = 0;
-
-            foreach (int item in primesList)
-            {
-                finalSum += item;
-            }
-
-            Console.WriteLine(finalSum);
+            primesList.Add(i);
         }
     }
+    GetSumOfPrimes();
+}
+
+bool isPrime(int number)
+{
+    if (number == 1) return false;
+    if (number == 2) return true;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 2; i <= boundary; ++i)
+    {
+        if (number % i == 0) return false;
+    }
+
+    return true;
+}
+
+void GetSumOfPrimes()
+{
+    int finalSum = 0;
+
+    foreach (int item in primesList)
+    {
+        finalSum += item;
+    }
+
+    Console.WriteLine(finalSum);
 }
